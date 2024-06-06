2b7813c582326b952d4c3a4fdbdcfdb27ea46132
Sohan Thummala <143735991+s0han24@users.noreply.github.com>
2024-06-07 03:50:21 +0530

    updated draw_box_static.py

--- draw_box_static.py
+++ draw_box_static.py
@@ -1,26 +1,28 @@
-from sh_stuff import connect
 import cv2
 from ultralytics import YOLO
 
-model_logo = YOLO("models\\brand logo\\best.pt")
-model_object = YOLO("models\\shelf detection\\best.pt")
-
-names = model_logo.names
+model = YOLO("models\\shelf detection\\best.pt")
 
 def draw_bounding_boxes(image, results):
+    
     # Extract bounding boxes, classes, names, and confidences
-
-    i = 0
+    boxes = results[0].boxes.xyxy.tolist()
+    classes = results[0].boxes.cls.tolist()
+    names = results[0].names
+    confidences = results[0].boxes.conf.tolist()
+    print(len(results))
     # Iterate through the results
-    for box1, box2 in results:
-        x_min_obj, y_min_obj, x_max_obj, y_max_obj = box1
-        x_min_logo, y_min_logo, x_max_logo, y_max_logo = box2
+    for box, cls, conf in zip(boxes, classes, confidences):
+        x_min, y_min, x_max, y_max = box
+        print(box)
+        confidence = conf
+        name = names[int(cls)]
         
-        image = cv2.rectangle(image, (int(x_min_obj), int(y_min_obj)), (int(x_max_obj), int(y_max_obj)), (i, i, i), 5)
-        image = cv2.rectangle(image, (int(x_min_logo), int(y_min_logo)), (int(x_max_logo), int(y_max_logo)), (i, i, i), 5)
+        
+        # Draw rectangle on the image
+        image = cv2.rectangle(image, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 255, 0), 2)
         # Optionally, put class_id text
-        image = cv2.putText(image, 'logo',(int(x_min_logo), int(y_min_logo - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-        i+=10
+        image = cv2.putText(image, str(name) + ' ' + str(confidence),(int(x_min), int(y_min - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
     # image = cv2.flip(image, 1)
     # Display the image
@@ -29,12 +31,13 @@ def draw_bounding_boxes(image, results):
     
 
 while True:
-    frame = cv2.imread("can.jpeg")
-    results_logos = model_logo(frame)
-    results_objects = model_object(frame)
-    results = connect(results_objects[0].boxes.xyxy.tolist(), results_logos[0].boxes.xyxy.tolist(), 0.5)
+    frame = cv2.imread("46_QS2817970_AddVisibilityImg-20240423_101925.jpg")
+    results = model([frame])
     draw_bounding_boxes(frame, results)
     if cv2.waitKey(1) == ord('q'):
         break
+
+
+
 
 

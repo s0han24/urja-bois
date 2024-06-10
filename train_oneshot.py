9c4992453fc8cc6092d080c5846b0ac519a1b838
Sohan Thummala <143735991+s0han24@users.noreply.github.com>
2024-06-10 17:19:43 +0530

    Update train_oneshot.py

--- train_oneshot.py
+++ train_oneshot.py
@@ -1,28 +1,73 @@
-import tensorflow as tf
-import pandas as pd
+import torch
+from torchvision import datasets, models, transforms
+import torch.nn as nn
+import os
 
-# Load the dataset
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-    # "D:\\detect_label\\flickr_logos_27_dataset\\classify_by_brand_dataset",
-    "D:\\detect_label\\flickr_logos_27_dataset\\test1_data",
-    image_size=(224, 224),
-)
-res_model = tf.keras.applications.ResNet50(include_top=False, weights="imagenet", input_shape=(224, 224, 3))
-for layer in res_model.layers:
-    layer.trainable = False
-x = res_model.output
-x = tf.keras.layers.GlobalAveragePooling2D()(x)
-x = tf.keras.layers.Dense(512, activation='relu')(x) 
-x = tf.keras.layers.Dropout(0.5)(x)
-x = tf.keras.layers.Dense(256, activation='relu')(x) 
-x = tf.keras.layers.Dropout(0.5)(x)
-x = tf.keras.layers.Dense(128, activation='relu')(x) 
-x = tf.keras.layers.Dropout(0.5)(x)
-x = tf.keras.layers.Dense(64, activation='relu')(x) 
-x = tf.keras.layers.Dropout(0.5)(x)
-predictions = tf.keras.layers.Dense(2, activation='softmax')(x)
-model = tf.keras.models.Model(inputs=res_model.input, outputs=predictions)
-model.summary()
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-history = model.fit(train_ds, epochs=20, verbose=1)
-model.save("model.onnx", save_format="onnx")
+transform = transforms.Compose([        # Defining a variable transforms
+ transforms.Resize(256),                # Resize the image to 256×256 pixels
+ transforms.CenterCrop(224),            # Crop the image to 224×224 pixels about the center
+ transforms.ToTensor(),                 # Convert the image to PyTorch Tensor data type
+ transforms.Normalize(                  # Normalize the image
+ mean=[0.485, 0.456, 0.406],            # Mean and std of image as also used when training the network
+ std=[0.229, 0.224, 0.225]      
+)])
+
+data_dir = 'test1'
+
+model = models.resnext101_64x4d(pretrained = True)
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 1)
+model = nn.Sequential(model, nn.Sigmoid())
+
+loss_fn = torch.nn.BCELoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+
+import pandas as pd
+import PIL.Image as Image
+
+df = pd.read_csv("image_classification.csv")
+
+def train_one_epoch():
+    avg_loss = 0.
+    total_loss=0
+    # Here, we use enumerate(training_loader) instead of
+    # iter(training_loader) so that we can track the batch
+    # index and do some intra-epoch reporting
+    for i, data in df.iterrows():
+        # Every data instance is an input + label pair
+        input, label = data
+        
+        # Zero your gradients for every batch!
+        optimizer.zero_grad()
+        
+        # Make predictions for this batch
+        img = transform(Image.open(input))
+        format_img = torch.unsqueeze(img, 0)
+        outputs = model(format_img)
+
+        # Compute the loss and its gradients
+        loss = loss_fn(outputs, torch.unsqueeze(torch.tensor([float(label)]),0))
+        loss.backward()
+
+        print("image {}, loss = {}".format(i, loss.item()))
+        total_loss += loss.item()
+        # Adjust learning weights
+        optimizer.step()
+    avg_loss = total_loss/df.shape[0]
+    return avg_loss
+
+epoch_number = 0
+EPOCHS = 20
+
+for epoch in range(EPOCHS):
+    print('EPOCH {}:'.format(epoch_number + 1))
+    
+    # Make sure gradient tracking is on, and do a pass over the data
+    model.train(True)
+    avg_loss = train_one_epoch()
+    print('Loss: {}'.format(avg_loss))
+    model.train(False)
+    epoch_number += 1
+
+# Save the model
+torch.save(model, 'model.pt')

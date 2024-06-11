c7abb1d2497c6775c70e8b820628580a525f7b82
Hayden-Soares <130130219+Hayden-Soares@users.noreply.github.com>
2024-06-11 10:45:19 +0530

    Update split_by_brand.py

--- split_by_brand.py
+++ split_by_brand.py
@@ -7,7 +7,7 @@ def classify_image(image_path, class_label):
         file.write(f"{image_path},{class_label}\n")
 
 # Path to the folder
-folder = "test1"
+folder = "split_dataset"
 
 # Iterate through the images in the folder
 for image_file in os.listdir(folder):
@@ -16,17 +16,17 @@ for image_file in os.listdir(folder):
     # Display the image
     print(f"Displaying image: {image_path}")
     cv2.imshow("Image", cv2.imread(image_path))
-    cv2.waitKey(0)
+    user_input = cv2.waitKey(0)
 
     # Ask the user for input (w, a, or d)
-    user_input = input(f"Classify image {image_file} as (w) himalaya, (a) not himalaya, or (d) garbage: ")
+    #user_input = input(f"Classify image {image_file} as (w) himalaya, (a) not himalaya, or (d) garbage: ")
 
     # Classify the image based on user input
-    if user_input == "w":
+    if user_input == ord("w"):
         classify_image(image_path, "himalaya")
-    elif user_input == "a":
+    elif user_input == ord("a"):
         classify_image(image_path, "not himalaya")
-    elif user_input == "d":
+    elif user_input == ord("d"):
         pass
     else:
         print("Invalid input. Skipping the image.")
@@ -36,6 +36,6 @@ print("Image classification completed.")
 import pandas as pd
 import shutil
 
-df = pd.read_csv("image_classification.csv")
-for i, row in df.iterrows():
-    shutil.copyfile(os.path.join(row['image_path']), os.path.join("test1_data", "training", str(row['class_label']), row['image_path'].split("\\")[-1]))
+#df = pd.read_csv("image_classification.csv")
+#for i, row in df.iterrows():
+    #shutil.copyfile(os.path.join(row['image_path']), os.path.join("test1_data", "training", str(row['class_label']), row['image_path'].split("\\")[-1]))
